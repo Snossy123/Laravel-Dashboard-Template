49768dd1b77095550209b13dcdab16b2f943f0c4
Solieman <soliemansnossy@gmail.com>
2023-08-23 17:36:11 +0200

    upload project

---
+++ resources/views/empty_dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.master')
+
+@section('title')
+    [page title]
+@endsection
+
+@section('css')
+@endsection
+
+@section('section')
+    [Title]
+@endsection
+
+@section('subsection')
+    [SubTitle]
+@endsection
+
+@section('content')
+@endsection
+
+@section('scripts')
+@endsection
